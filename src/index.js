1db8c6a2ffe2c75097545d26fa8575be7957a12c
Chester Heng <chester.heng@gmail.com>
2019-12-24 18:41:39 +0800

    lab 02:The functions

--- src/index.js
+++ src/index.js
@@ -11,3 +11,16 @@ console.log(firstName);
 console.log(lastName);
 console.log(age);
 console.log(hasDrivingLicence);
+
+// Lab 2: The functions
+
+const display = (firstName, lastName) => {
+  return firstName + " " + lastName.toUpperCase();
+};
+
+const displayTemplate = (firstName, lastName) => {
+  return `${firstName} ${lastName.toUpperCase()}`;
+};
+
+console.log(display(firstName, lastName));
+console.log(displayTemplate(firstName, lastName));
