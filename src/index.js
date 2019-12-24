199d9f833b24198400b4e2450990a0a3d879dffb
Chester Heng <chester.heng@gmail.com>
2019-12-24 18:29:32 +0800

    lab 01:Data types and variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('Hello ');
+
+// Lab 1: Data types and variables
+
+const firstName = 'John';
+const lastName = 'Doe';
+const age = 20;
+const hasDrivingLicence = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hasDrivingLicence);
